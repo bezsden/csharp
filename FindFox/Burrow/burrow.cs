82284a447b131b47c3581fb7113f176a68ab8045
bezsden <denis.bezsmertnyi@sprinterra.com>
2020-03-05 13:41:05 +0200

    initial burrow

---
+++ FindFox/Burrow/burrow.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindFox.Burrow
+{
+    public class Burrow
+    {
+        public string BurrowName {get; set;}
+    }
+}
--- FindFox/Program.cs
+++ FindFox/Program.cs
@@ -6,7 +6,7 @@ namespace FindFox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
